5800073f581fb662efba926b160ca4432479db76
ChangleWei <cw3755@columbia.edu>
2026-02-27 00:23:11 -0500

    Implement bond duration calculation logic
    Refactor getBondDuration to calculate duration based on inputs.

--- BondDuration_File.py
+++ BondDuration_File.py
@@ -1,8 +1,19 @@
 import numpy as np
 
 def getBondDuration(y, face, couponRate, m, ppy = 1):
-    if ppy == 1:
-        x = 8.51
-    if ppy == 2:
-        x = 8.42
-    return(x)
+    n = m * ppy
+    r = y / ppy
+    c = face * couponRate / ppy
+
+    t = np.arange (1, n +1)
+    discount = 1 / ((1 + r) ** t)
+
+    cf = np.full (n, c)
+    cf[-1] = cf[-1] + face
+
+    pvcf = cf * discount
+    pvcf_sum = np.sum(pvcf)
+
+    ty = t / ppy
+    duration = np.sum(ty * pvcf) / pvcf_sum
+    return(duration)
