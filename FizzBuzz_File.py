95aaa4b4989eaa9c97b1bfbd9c327a517bc999f3
ChangleWei <cw3755@columbia.edu>
2026-02-27 00:45:10 -0500

    Refactor FizzBuzz function to use numpy arrays

--- FizzBuzz_File.py
+++ FizzBuzz_File.py
@@ -1,15 +1,14 @@
 import numpy as np
 
 def FizzBuzz(start, finish):
-    n = np.arange(start, finish +1)
-    out = []
+    numvec = np.arange(start, finish +1)
+    out = np.array(numvec, dtype = object)
 
-    n3 = (n % 3 ==0)
-    n5 = (n % 5 ==0)
+    n3 = (numvec % 3 ==0)
+    n5 = (numvec % 5 ==0)
 
     out[n3] = "fizz"
     out[n5] = "buzz"
     out[n3 & n5] = "fizzbuzz"
-    out[n] = n
     
-    return(out)
+    return (out)
